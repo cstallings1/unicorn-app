6efa818f4dc29ad013fa2e5ed31501c4f35b9a5e
cstallings1 <catiestallings1@gmail.com>
2016-04-06 18:32:09 -0500

    Unicorn gifs now appearing on screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
-angular.module('starter', ['ionic'])
+angular.module('starter.controllers', [])
 
-.controller('DisplayUnicornCtrl', function($scope, $http) {
-  $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC').then(function(resp) {
-    console.log('Success', resp);
-  }, function(err) {
-    console.log('ERR', err);
-  })
+.controller('UnicornCtrl', function($scope, Unicorns) {
+  // $scope.gifs = Unicorns;
+  Unicorns.success(function(data) {
+     $scope.gifs = data.data;
+   })
 })
+
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,14 @@
-angular.module('starter.services', ['ionic'])
-.factory('Unicorns', function() {
-  var gifs = {
-    unicornGifs: []
-  }
+angular.module('starter.services', [])
+
+.factory('Unicorns', function($http) {
+  return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC').success(function(resp) {
+    var unicornGifs = resp.data;
+    console.log(unicornGifs)
+    return unicornGifs;
+  });
 });
+
+
+
+
+
