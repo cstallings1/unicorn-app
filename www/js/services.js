760b9ce3656ce65bf0e9c31fd1689caa097303c8
cstallings1 <catiestallings1@gmail.com>
2016-04-07 18:25:16 -0500

    Working on adding defer but still hasn't fixed the infinite scroll

--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,36 @@
 angular.module('starter.services', [])
 
-.factory('Unicorns', function($http) {
+.factory('Unicorns', function($http, $q) {
   var unicornGifs = [];
 
   return {
     GetFeed: function(){
+      var deferred = $q.defer();
+
       return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&limit=10&api_key=dc6zaTOxFJmzC').then(function(response){
         unicornGifs = response.data;
-        return unicornGifs;
+        deferred.resolve(unicornGifs);
+
+        // return unicornGifs;
       });
+      return deferred.promise;
     },
     GetMoreGifs: function(){
-      return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&limit=10&offset=' + unicornGifs.length + '&api_key=dc6zaTOxFJmzC').then(function(response){
+      var deferred = $q.defer();
 
-        // unicornGifs = unicornGifs.concat(response.data);
-
-        unicornGifs = response.data;
-        return unicornGifs;
+      return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&limit=10&offset=' + unicornGifs.data.length + '&api_key=dc6zaTOxFJmzC').then(function(response){
+        unicornGifs = unicornGifs.data.concat(response.data);
+        deferred.resolve(unicornGifs);
+        // unicornGifs = response.data;
+        // return unicornGifs;
       });
+      return deferred.promise;
     }
   }
 
 
 });
+
+
+
 
