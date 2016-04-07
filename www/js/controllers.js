ee5ab5149bd24f7c530c467fa98ba09cdc9638f2
cstallings1 <catiestallings1@gmail.com>
2016-04-07 17:24:52 -0500

    Refactor controller and services file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('UnicornCtrl', function($scope, Unicorns) {
-  Unicorns.success(function(data) {
-     $scope.gifs = data.data;
-   })
-})
+// .controller('UnicornCtrl', function($scope, Unicorns) {
+//   Unicorns.success(function(data) {
+//      $scope.gifs = data.data;
+//    })
+// })
 
+.controller('UnicornCtrl', function($scope, Unicorns) {
+  $scope.gifs = [];
+
+  Unicorns.GetFeed().then(function(unicornGifs) {
+     $scope.gifs = unicornGifs.data;
+     console.log($scope.gifs);
+   });
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,53 @@
 angular.module('starter.services', [])
 
+// .factory('Unicorns', function($http) {
+//   return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&limit=10&api_key=dc6zaTOxFJmzC').success(function(resp) {
+//     var unicornGifs = resp.data;
+//     return unicornGifs;
+//   });
+// });
+
 .factory('Unicorns', function($http) {
-  return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC').success(function(resp) {
-    var unicornGifs = resp.data;
-    return unicornGifs;
-  });
+  var unicornGifs = [];
+
+  return {
+    GetFeed: function(){
+      return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&limit=10&api_key=dc6zaTOxFJmzC').then(function(response){
+        unicornGifs = response.data;
+        return unicornGifs;
+      });
+    }
+  //   GetNewUsers: function(){
+  //     return $http.get(BASE_URL+'?results=10').then(function(response){
+  //       items = response.data.results;
+  //       return items;
+  //     });
+  //   }
+  }
+
+
 });
+
+
+// .factory('PersonService', function($http){
+//   var BASE_URL = "http://api.randomuser.me/";
+//   var items = [];
+
+//   return {
+//     GetFeed: function(){
+//       return $http.get(BASE_URL+'?results=10').then(function(response){
+//         items = response.data.results;
+//         return items;
+//       });
+//     },
+//     GetNewUsers: function(){
+//       return $http.get(BASE_URL+'?results=10').then(function(response){
+//         items = response.data.results;
+//         return items;
+//       });
+//     }
+//   }
+// })
 
 
 
