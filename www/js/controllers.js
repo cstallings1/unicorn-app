2361182ce0ba086cec70e21c2161db56484fb2c2
cstallings1 <catiestallings1@gmail.com>
2016-04-07 17:08:11 -0500

    Format gifs on main tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,6 @@
 angular.module('starter.controllers', [])
 
 .controller('UnicornCtrl', function($scope, Unicorns) {
-  // $scope.gifs = Unicorns;
   Unicorns.success(function(data) {
      $scope.gifs = data.data;
    })
--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,6 @@ angular.module('starter.services', [])
 .factory('Unicorns', function($http) {
   return $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC').success(function(resp) {
     var unicornGifs = resp.data;
-    console.log(unicornGifs)
     return unicornGifs;
   });
 });
